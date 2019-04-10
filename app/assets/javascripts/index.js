1fb1b48c3d93b57413fec151d405f5c79e60713b
hama-ei <eri_no_pasokon@yahoo.co.jp>
2019-04-10 15:43:01 +0900

    create-incrementalsearch
    インクリメンタルサーチの実装

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,5 +1,53 @@
 $(function() {
+  function appendUser(user) {
+   var html = `<div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">${user.name}</p>
+                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}", data-user-name="${user.name}">追加</a>
+              </div>`
+
+    $("#user-search-result").append(html);
+  };
+
+  function changeUser(user_id,user_name) {
+    var html =`<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                <input name='group[user_ids][]' type='hidden' value="${user_id}">
+                <p class='chat-group-user__name'>${user_name}</p>
+                <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+              </div>`
+
+    $("#chat-group-user").append(html);
+  };
+
   $(".chat-group-form__input").on("keyup", function() {
     var input = $(".chat-group-form__input").val();
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+   .done(function(users) {
+    $("#user-search-result").empty();
+      if (users.length !== 0) {
+        users.forEach(function(user){
+         appendUser(user);
+        });
+      }
+    })
+   .fail(function() {
+      alert('ユーザー検索に失敗しました');
+    });
   });
+
+  $(document).on('click', '.user-search-add', function(){
+    $(this).parent().remove();
+    var user_id = $(this).attr('data-user-id');
+    var user_name = $(this).attr('data-user-name');
+     changeUser(user_id,user_name);
+  });
+
+  $(document).on('click', '.user-search-remove', function(){
+    $(this).parent().remove();
+  })
 });
+

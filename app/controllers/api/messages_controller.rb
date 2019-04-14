2c86a4554a9d95820dd5ab75ef553db19508135d
hama-ei <eri_no_pasokon@yahoo.co.jp>
2019-04-14 21:06:40 +0900

    correction
    messagesを限定した

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,8 +1,9 @@
 class Api::MessagesController < ApplicationController
   def index
+    @group = Group.find(params[:group_id])
     respond_to do |format|
       format.html
-      format.json { @new_messages = Message.where('id > ?', params[:id]) }
+      format.json { @new_messages =  @group.messages.where('id > ?', params[:id]) }
       # json形式でアクセスがあった場合は、params[:message][:id]よりも大きいidがないかMessageから検索して、@new_messageに代入する
     end
   end

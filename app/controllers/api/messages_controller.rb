60fd29fce86e81b644488bdafab9470d4eb19ab1
hama-ei <eri_no_pasokon@yahoo.co.jp>
2019-04-14 20:05:16 +0900

    add-automatic_update_function
    自動更新機能の実装

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,10 +1,9 @@
 class Api::MessagesController < ApplicationController
-
   def index
-    @messages = Message.all
     respond_to do |format|
       format.html
-      format.json { @new_message = Message.where('id > ?', params[:message][:id]) } # json形式でアクセスがあった場合は、params[:message][:id]よりも大きいidがないかMessageから検索して、@new_messageに代入する
+      format.json { @new_messages = Message.where('id > ?', params[:id]) }
+      # json形式でアクセスがあった場合は、params[:message][:id]よりも大きいidがないかMessageから検索して、@new_messageに代入する
     end
   end
 end

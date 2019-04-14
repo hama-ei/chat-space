4679e9a9b7fba47d4b925333be2b7a4579c0bbe3
hama-ei <eri_no_pasokon@yahoo.co.jp>
2019-04-14 22:16:51 +0900

    correction
    ・buildMessageHTMLの記述の変更
    ・reset→valへ戻す

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -38,9 +38,9 @@ $(function(){
     })
     .done(function(data){
       var html = buildHTML(data);
-      $('.messages').append(html)
-      $('.new_message__box').reset()
-      $('.hidden').reset()
+      $('.messages').append(html);
+      $('.new_message__box').val("")
+      $('.hidden').val("")
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
     })
     .fail(function(){
@@ -52,7 +52,9 @@ $(function(){
   })
 
 var buildMessageHTML = function(message) {
-    if (message.content && message.image.url) {
+  var content = message.content ? `${ message.content }` : "";
+  var image = message.image.url ? `<img src= ${ message.image.url }>` : "";
+
       //data-idが反映されるようにしている
       var html = '<div class="message" data-id=' + message.id + '>' +
         '<div class="upper-message">' +
@@ -70,39 +72,6 @@ var buildMessageHTML = function(message) {
           '<img src="' + message.image.url + '" class="lower-message__image" >' +
         '</div>' +
       '</div>'
-    } else if (message.content) {
-      //同様に、data-idが反映されるようにしている
-      var html = '<div class="message" data-id=' + message.id + '>' +
-        '<div class="upper-message">' +
-          '<div class="upper-message__user-name">' +
-            message.user_name +
-          '</div>' +
-          '<div class="upper-message__date">' +
-            message.created_at +
-          '</div>' +
-        '</div>' +
-        '<div class="lower-message">' +
-          '<p class="lower-message__content">' +
-            message.content +
-          '</p>' +
-        '</div>' +
-      '</div>'
-    } else if (message.image.url) {
-      //同様に、data-idが反映されるようにしている
-      var html = '<div class="message" data-id=' + message.id + '>' +
-        '<div class="upper-message">' +
-          '<div class="upper-message__user-name">' +
-            message.user_name +
-          '</div>' +
-          '<div class="upper-message__date">' +
-            message.created_at +
-          '</div>' +
-        '</div>' +
-        '<div class="lower-message">' +
-          '<img src="' + message.image.url + '" class="lower-message__image" >' +
-        '</div>' +
-      '</div>'
-    };
     return html;
   };
 
